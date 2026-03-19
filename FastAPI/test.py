83f6b2e4990fdc8dc8d9f32cb66ffebf54eb7228
Jkzmr <96251811+JanKazimir@users.noreply.github.com>
2026-03-19 16:42:32 +0100

    progress on API

--- FastAPI/test.py
+++ FastAPI/test.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class DoubleInput(BaseModel):
+    number: int
+
+
+@app.post("/double/")
+def ask_for_double(num: DoubleInput):
+    return {"Your number doubled" : {num.number *2}}
+    
 
 @app.get("/")
 async def root():
     return {"message": "testing World"}
 
+@app.get("/double/{number}")
+def double(number :int):
+    return {"Your number doubled" : {number*2}}
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app)
--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class DoubleInput(BaseModel):
+    number: int
+
+@app.post("/double/")
+def ask_for_double(num: DoubleInput):
+    return {"Your number doubled" : num.number *2}
+    
+
+class Salary(BaseModel):
+    salary: int
+    bonus : int
+    taxes: int
+
+
+@app.post("/salary/")
+def final_salary_calculation(sal: Salary):
+    final_pay = sal.salary + sal.bonus - sal.taxes
+    return {"final pay:": final_pay}
+
+        
+        
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/double/{number}")
+def double(number :int):
+    return {"Your number doubled" : {number*2}}
 
 
-
-def bal():
-    
-
-
-if __name__ == "__main__":
-    pass
